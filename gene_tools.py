151a2aef5dbd3ff29b899777dcba9808b6996f35
Jerosmy <79279531+Jerosmy@users.noreply.github.com>
2025-05-17 20:39:23 +0200

    update compute_scores

--- gene_tools.py
+++ gene_tools.py
@@ -71,8 +71,22 @@ def compute_scores(df):
     Returns:
         pd.DataFrame: Dataframe with added Score, Rank, and Percentile columns.
     """
-    perc_cols = [col for col in df.columns if col.endswith('_percentile')]
-    df['Score'] = df[perc_cols].mean(axis=1)
+    if "Method" in df.columns and "p_value" in df.columns and "b_ivw" in df.columns():
+        method_name= df["Method"].iloc[0]
+        if method_name in ["eQTL_GWAS_blood","pQTL-GWAS"]:
+            threshold = 0.05/len(df.rows())
+            my_betas = df["p_value"] <= threshold
+            my_pvals = df["p_value"] > threshold
+            ranking_betas = df.loc[my_betas,"b_ivw"].abs().rank(method="min",ascending=False)
+            ranking_pvalues = df.loc[my_pvals,"p_value"].rank(method="min",ascending=True)
+            ranking_pvalues +=len(ranking_betas)
+            
+            bothranks =pd.concat([ranking_betas,ranking_pvalues])
+            df["Score"] =bothranks.sort_index() / len(df)
+        else:
+            perc_cols = [col for col in df.columns if col.endswith('_percentile')]
+            df['Score'] = df[perc_cols].mean(axis=1)
+
     df['Rank'] = df['Score'].rank()
     df['Percentile'] = (df['Rank'] / len(df)) * 100
     return df
