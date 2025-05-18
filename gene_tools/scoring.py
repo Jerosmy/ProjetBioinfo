7a5fc64ee30ced0e0b368a5af075893c2272cfd7
ScubaSlavvo <ptitbarbar@gmail.com>
2025-05-18 16:35:16 +0200

    j'espère que ça marche

--- gene_tools/scoring.py
+++ gene_tools/scoring.py
@@ -11,25 +11,50 @@ def compute_scores(df):
     Returns:
         pd.DataFrame: Dataframe with added Score, Rank, and Percentile columns.
     """
-    if "Method" in df.columns and "p_value" in df.columns and "b_ivw" in df.columns():
-        method_name= df["Method"].iloc[0]
-        if method_name in ["eQTL_GWAS_blood","pQTL-GWAS"]:
-            threshold = 0.05/len(df.rows())
-            my_betas = df["p_value"] <= threshold
-            my_pvals = df["p_value"] > threshold
-            ranking_betas = df.loc[my_betas,"b_ivw"].abs().rank(method="min",ascending=False)
-            ranking_pvalues = df.loc[my_pvals,"p_value"].rank(method="min",ascending=True)
-            ranking_pvalues +=len(ranking_betas)
+
+    perc_cols = [col for col in df.columns if col.endswith('_percentile')]
+
+    df["NA_Count"] = df[perc_cols].isna().sum(axis=1)
+
+    if "Method" in df.columns and "p_value" in df.columns and "b_ivw" in df.columns:
+         method_name= df["Method"].iloc[0]
+         if method_name in ["eQTL_GWAS_blood","pQTL-GWAS"]:
+             threshold = 0.05/len(df.rows())
+
+             my_betas = df["p_value"] <= threshold
+             my_pvals = df["p_value"] > threshold
+
+             ranking_betas = df.loc[my_betas,"b_ivw"].abs().rank(method="min",ascending=False)
+             ranking_pvalues = df.loc[my_pvals,"p_value"].rank(method="min",ascending=True)
+             ranking_pvalues +=len(ranking_betas)
+             
+             bothranks =pd.concat([ranking_betas,ranking_pvalues])
+             df["Score"] =bothranks.sort_index() / len(df)
+         else:
             
-            bothranks =pd.concat([ranking_betas,ranking_pvalues])
-            df["Score"] =bothranks.sort_index() / len(df)
-        else:
-            perc_cols = [col for col in df.columns if col.endswith('_percentile')]
+    
             df['Score'] = df[perc_cols].mean(axis=1)
 
     df['Rank'] = df['Score'].rank()
     df['Percentile'] = (df['Rank'] / len(df)) * 100
     return df
+
+
+
+def NA_filtering(df, max_na=0):
+    """
+    Filter out rows with more than `max_na` missing scores.
+
+    Argsument:
+        df (pd.DataFrame): Scored dataframe containing 'NA_Count'.
+        max_na (int): Maximum allowed number of NAs per row.
+
+    Returns:
+        pd.DataFrame: Filtered dataframe.
+    """
+
+    return df[df["NA_Count"] <= max_na].copy()
+
 
 
 def geneScores(all_data):
